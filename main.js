174154d26320b6dbd3c127e9d8e630122689a129
unknown <maalma0205@gmail.com>
2020-11-24 19:26:37 -0500

    Operadores de asignacion

---
+++ main.js
@@ -0,0 +1,77 @@
+/*let numero_1 = 5;
+let numero_2 = 10;
+
+
+console.log("contenido de la variable 1 nombre de la variable numero_1: ",numero_1);
+console.log("contenido de la variable 2 nombre de la variable numero_2: ",numero_2);
+
+numero_1 = numero_2
+
+console.log("la variable 1 numero_1 contiene el mismo valor que la variable 2 numero_2");
+
+console.log("contenido de la variable 1 nombre de la variable numero_1: " ,numero_1);
+console,log("contenido de la variable 2 nombre de la variable numero_2: " ,numero_2);*/
+
+
+/*operacion suma
+let numero_1 = 2;
+let numero_2 = 30;
+
+numero_1 += numero_2;
+resultado = numero_1 += numero_2;
+
+console.log(numero_1);*/
+
+
+/*Operacion resta
+let numero_1 = 2;
+let numero_2 = 30;
+
+numero_2 -= numero_1;
+
+console.log(numero_2);*/
+
+
+/*Operacion multiplicaciones
+let numero_1 = 2;
+let numero_2 = 30;
+
+numero_1 *= numero_2;
+
+console.log(numero_1);*/
+
+
+//Operacion dividir
+/*let numero_1 = 2;
+let numero_2 = 30;
+
+numero_2 /= numero_1;
+
+console.log(numero_2);*/
+
+//Operacion porcentaje
+/*let numero_1 = 2;
+let numero_2 = 30;
+let resultado;
+
+resultado = numero_2 / numero_1;
+console.log(resultado);
+resultado = numero_2 %= numero_1;
+console.log(resultado);*/
+
+//operacion exponenciacion
+/*/let numero_1 = 2;
+let numero_2 = 30;
+
+numero_2 **= numero_1;
+
+console.log(numero_2);*/
+
+
+//asignacion desplazamiento a la izquierda
+/*let numero_1 = 2;
+let numero_2 = 30;
+
+numero_1 &= numero_2;
+
+console.log(numero_2);*/
